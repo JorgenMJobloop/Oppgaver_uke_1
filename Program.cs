5796da9cb5095dbc63a845377fc5959ac3f11647
jorgenmjobloop <jorgen@jobloop.no>
2024-09-06 10:39:59 +0200

    Added more key/value pairs in the dictionary implementation

--- Program.cs
+++ Program.cs
@@ -16,15 +16,15 @@ class Program
         Console.WriteLine(myDouble);
         char myChar = 'O';
         Console.WriteLine(myChar);
-        int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+        int[] myIntArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-        for(int i = 0; i < myIntArray.Count(); i++) 
+        for (int i = 0; i < myIntArray.Count(); i++)
         {
             Console.WriteLine(myIntArray[i]);
         }
 
-        string[] myStringArray = {"Hello", "From", "A", "C#", "Array!"};
-        foreach(string myStrings in myStringArray) 
+        string[] myStringArray = { "Hello", "From", "A", "C#", "Array!" };
+        foreach (string myStrings in myStringArray)
         {
             Console.WriteLine(myStrings);
         }
@@ -36,7 +36,7 @@ class Program
         myList.Add("A");
         myList.Add("C#");
         myList.Add("List");
-        foreach(string strings in myList) 
+        foreach (string strings in myList)
         {
             Console.WriteLine(strings);
         }
@@ -46,34 +46,36 @@ class Program
 
         myDictionary.Add(1, "Value 1");
         myDictionary.Add(2, "Value 2");
+        myDictionary.Add(3, "Value 3");
+        myDictionary.Add(4, "Value 4");
 
-        foreach(var keyvaluepair in myDictionary) 
+        foreach (var keyvaluepair in myDictionary)
         {
             Console.WriteLine(keyvaluepair);
         }
 
-        Console.WriteLine(AddNumbers(5,5));
-        Console.WriteLine(SayHello("Kai"));
+        Console.WriteLine(AddNumbers(5, 5));
+        Console.WriteLine(SayHello("Jørgen"));
 
-        double MyDoubleMethod(int a, double b) 
+        double MyDoubleMethod(int a, double b)
         {
             return a + b;
         }
-        Console.WriteLine(MyDoubleMethod(3,2.5));
+        Console.WriteLine(MyDoubleMethod(3, 2.5));
 
-        string MyStringMethod(string message) 
+        string MyStringMethod(string message)
         {
             return message;
         }
         Console.WriteLine(MyStringMethod("Hello MyStringMethod!"));
     }
-// Eksempler på metoder som ligger utenfor static void Main() sitt scope
-    static int AddNumbers(int a, int b) 
+    // Eksempler på metoder som ligger utenfor static void Main() sitt scope
+    static int AddNumbers(int a, int b)
     {
         return a + b;
     }
 
-    static string SayHello(string greeting) 
+    static string SayHello(string greeting)
     {
         return "Hello there " + greeting + "!";
     }
